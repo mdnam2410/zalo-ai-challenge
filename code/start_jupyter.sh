8000e29bdd3698db177cd810e9a55f86a9d03f33
Nam Mai <mdnam2410@gmail.com>
2022-11-25 23:50:13 +0700

    Fix spleet (squashed)
    Extract vocals function
    
    - Make speech_file_to_array_fn to extract vocals
    - Add 2 more required libraries [ffmpeg (version 1.4), spleeter (version 2.3.2)]
    
    Update predict.py
    
    - Delete hard code for sample_rate
    - Don't use sample_rate parameter on audio_loader.load function
    
    Update predict.py
    
    - add resampler.orig_freq
    
    Update shell scripts to activate virtual environment before running
    
    start_jupyter: return to root after activating virtual environment

--- code/start_jupyter.sh
+++ code/start_jupyter.sh
@@ -1,1 +1,9 @@
+CODE_DIR='/code'
+echo "cd into $CODE_DIR and activate virtual environment"
+cd $CODE_DIR
+source bin/activate
+
+echo "Return to root"
+cd /
+
 jupyter lab --port 9777 --ip 0.0.0.0 --NotebookApp.password='zac2022' --NotebookApp.token='zac2022' --allow-root --no-browser
