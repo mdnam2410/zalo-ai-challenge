9fef403ff8fdef7c6c790b370d0a0b66f08d5db5
Nam Mai <mdnam2410@gmail.com>
2022-11-27 16:25:33 +0700

    Update shell scripts to activate virtual environment before running

--- code/start_jupyter.sh
+++ code/start_jupyter.sh
@@ -1,1 +1,6 @@
+CODE_DIR='/code'
+echo "cd into $CODE_DIR and activate virtual environment"
+cd $CODE_DIR
+source bin/activate
+
 jupyter lab --port 9777 --ip 0.0.0.0 --NotebookApp.password='zac2022' --NotebookApp.token='zac2022' --allow-root --no-browser
